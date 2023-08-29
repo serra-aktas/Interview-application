907884f1f9c715328864d515005ae7a85c760e79
serraktas <20060386@stu.omu.edu.tr>
2023-08-29 14:37:40 +0300

    mülakat uygulaması
    Bilgisayar Bilimleri alanında bir doktora öğrencisine sorulan soru

---
+++ uygulama.py
@@ -0,0 +1,21 @@
+# uygulama: Aşağıdaki şekilde string değiştiren fonksiyon yazmak istiyoruz.
+
+# before: "hi my name is john and i am learning python"
+# after: "Hi mY NaMe iS JoHn aNd i aM LeArNiNg pYtHoN"
+
+def alternating(string):
+    new_string = ""
+    #girilen string'in indexlerinde gez.
+    for string_index in range(len(string)):
+        #index çift ise büyük harfe çevir.
+        if string_index % 2 == 0:
+            new_string += string[string_index].upper()
+         #index tek ise küçük harfe çevir.   
+        else:
+            new_string += string[string_index].lower()
+            
+    print(new_string)
+        
+alternating("hi my name is john and i am learning python")
+   
+
